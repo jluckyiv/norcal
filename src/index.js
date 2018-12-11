ad57bf941dee774ff0f9cd1f5b16b2a0f6138783
Jack Lucky <jack.lucky.iv@gmail.com>
2018-12-11 04:48:45 -0800

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import './main.css';
+import { Elm } from './Main.elm';
+import registerServiceWorker from './registerServiceWorker';
+
+Elm.Main.init({
+  node: document.getElementById('root')
+});
+
+registerServiceWorker();

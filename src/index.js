8c3361e8d55f61868fc91fb6f112d9b648502a79
Jack Lucky <jack.lucky.iv@gmail.com>
2018-12-11 05:02:15 -0800

    Remove main.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bulma/css/bulma.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import './main.css';
+// import './main.css';
 import { Elm } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
 
